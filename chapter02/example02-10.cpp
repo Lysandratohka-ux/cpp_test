a9fec72098557675fe2c8d51ac3d3e80ac06e605
Lysandra <Lysandra_tohka@icloud.com>
2026-09-22 06:31:10 +0800

    第一次提交

---
+++ chapter02/example02-10.cpp
@@ -0,0 +1,11 @@
+#include<cstdio>
+using namespace std;
+int main(){
+    int a,b,c,d,delta,e,f;
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    delta=(60*c+d)-(60*a+b);
+    e=delta/60;
+    f=delta%60;
+    printf("%d %d",e,f);
+    return 0;
+}
