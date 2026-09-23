7bbd5a90f7115e06b403e96fc6ff9ceafda936df
lysandra yatogami <lysandrayatogami@lysandradeMacBook-Neo.local>
2026-09-23 19:09:51 +0800

    更新第三章练习和 C++ 编译配置

--- 自学书本练习/第03章/example03-02.cpp
+++ 自学书本练习/第03章/example03-02.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    bool p1,p2;
+    p1=a%2==0;
+    p2=4<a&&a<=12;
+    cout << (p1 && p2)<<' ';
+    cout << (p1 || p2) << ' ';
+    cout << (p1!=p2) << ' ';
+    cout << (!p1&&!p2) << endl  ;
+    
 
+}
+    
--- 自学书本练习/第03章/example03-03.cpp
+++ 自学书本练习/第03章/example03-03.cpp
@@ -1,1 +1,11 @@
-
+#include <iostream>
+using namespace std;   
+int main() {
+    int x;
+    bool p1,p2,p3;
+    cin >> x;
+    p1=x%4==0;
+    p2=x%100!=0;
+    p3=x%400==0;
+    cout << (p1||p2&&p3) << endl;
+}
